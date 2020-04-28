372348ea366d9c9bb68691cce2c409f9cf6dae3c
Emmanuel Ivan <64347802+EmmanuelIvan@users.noreply.github.com>
2020-04-28 15:46:20 +0000

    Create arithmetic operators.py

---
+++ arithmetic operators.py
@@ -0,0 +1,14 @@
+Number = input("Introduce el primer numero: ")
+Number_two = input("Introduce el segundo numero: ")
+
+#Suma
+print(Number + Number_two)
+
+#Resta
+print(Number - Number_two)
+
+#Multiplicacion
+print(Number * Number_two)
+
+#Division
+print(Number / Number_two)
